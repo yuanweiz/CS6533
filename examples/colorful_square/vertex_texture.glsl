b1c5788710f7a83c2ab137ee51cc3fe678c0b658
Weizhe Yuan <yuanweiz1113@gmail.com>
2016-10-21 02:53:25 +0000

    hw2

--- examples/colorful_square/vertex_texture.glsl
+++
@@ -1,7 +0,0 @@
-attribute vec4 position;
-attribute vec2 texCoord;
-varying vec2 varyingTexCoord;
-void main() {
-    varyingTexCoord = texCoord;
-    gl_Position = position;
-}
--- examples/cube/fragment.glsl
+++
@@ -1,5 +0,0 @@
-varying vec4 varyingColor;
-void main() {
-    //gl_FragColor = vec4(1,0,0,1);
-    gl_FragColor = varyingColor;
-}
